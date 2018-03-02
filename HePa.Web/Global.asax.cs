fdcca846cf1eaf8925ccac0b877b6a49e465fe82
Kim Nguyen <ngkim623.yahoo.com@gmail.com>
2018-03-02 18:56:58 +0700

    sdljks sklfjs iodf

---
+++ HePa.Web/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using HePa.Core.ErrorHandling;
+
+namespace HePa.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                "Error",    // Route name
+                "{*url}",   // URL with parameters
+                new { controller = "Error", action = "Index" } // Parameter defaults
+            );
+        }
+
+        protected void Application_Start()
+        {
+            
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // Removing X-AspNetMvc-Version in Header respond
+            MvcHandler.DisableMvcResponseHeader = true;
+
+            AreaRegistration.RegisterAllAreas();
+        }
+
+        /// <summary>
+        /// Delete the existing error handler and Register the newly written global filter
+        /// </summary>
+        /// <param name="filters"></param>
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new CustomErrorHandler());
+
+        }
+    }
+}
---
+++ HePa.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HePa.Web.Startup))]
+namespace HePa.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
